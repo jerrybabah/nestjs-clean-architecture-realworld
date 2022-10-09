131b87572b4ea0096ede2478a6ec1b52aa3bb696
Jerry Baba <ghdtjs0417@gmail.com>
2022-10-09 15:39:07 +0900

    Add: clean architecture 뼈대

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
+import { AppController } from '@/interface/controllers';
 
 @Module({
   controllers: [AppController],
